b4da915d466bbfd7809fdf3221ad82a1ea2ee8b6
maxosen64 <maxosen64@gmail.com>
2021-01-04 10:03:19 +0800

    Update ALLOWED_HOST

--- onlinebarter/settings.py
+++ onlinebarter/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    'serene-tundra-55963.herokuapp.com/',
+    'serene-tundra-55963.herokuapp.com',
     '127.0.0.1'
 ]
 
